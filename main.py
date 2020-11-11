71e3da3d214ea3d0ac2679f8b9f29c7ea7026d5e
gamblingrepairman <awaite67@gmail.com>
2020-11-11 03:41:43 -0500

    part 3 and 4 done

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import logging
 from logging import FileHandler
 import os
 import sqlite3
+import datetime
 
 app = Flask(__name__)
 newlist = []
 
 
 def logfile_start():
-    file_handler = FileHandler(os.getcwd()+"/log.txt")
+    file_handler = FileHandler(os.getcwd() + "/log.txt")
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
@@ -42,17 +43,86 @@ def dashboard():
     con = sqlite3.connect('hw13.db')
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    cur.execute("INSERT OR IGNORE INTO student (student_id, student_first_name, student_last_name) VALUES (1,'John','Smith')")
-    cur.execute("INSERT OR IGNORE INTO quizzes (quiz_id, quiz_subject, quiz_question_amount, quiz_date) VALUES (1, \
-    'Python Basics', 5, 'February, 5th, 2015')")
-    cur.execute("select student.student_id, student.student_first_name, student.student_last_name, \
-                quizzes.quiz_id, quizzes.quiz_subject, quizzes.quiz_question_amount, quizzes.quiz_date \
-                from student, quizzes")
-
+    strs = ["select student.student_id, student.student_first_name, student.student_last_name from student",
+            "select quizzes.quiz_id, quizzes.quiz_subject, quizzes.quiz_question_amount, quizzes.quiz_date from quizzes"]
+    # cur.execute("select student.student_id, student.student_first_name, student.student_last_name from student")
+    cur.execute("select student.student_id, student.student_first_name, student.student_last_name from student")
+    con.commit()
     rows = cur.fetchall()
+    cur.execute(
+        "select quizzes.quiz_id, quizzes.quiz_subject, quizzes.quiz_question_amount, quizzes.quiz_date from quizzes")
+    con.commit()
+    rows2 = cur.fetchall()
     con.close()
-    return render_template("dashboard.html", rows=rows)
+    return render_template("dashboard.html", rows=rows, rows2=rows2)
 
+
+@app.route('/student/add', methods=['GET', 'POST'])
+def add():
+    con = sqlite3.connect('hw13.db')
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("SELECT student_id FROM student WHERE student_id=(SELECT max(student_id) FROM student)")
+    sid = [dict(id=row[0]) for row in cur.fetchall()][0]["id"] + 1
+    con.close()
+    if request.method == 'POST':
+        if request.form['student_name'] is not None:
+            try:
+                student_name = request.form['student_name']
+                student_name = student_name.split(' ')
+                sfn, sln = '"' + student_name[0].strip() + '"', '"' + student_name[1].strip() + '"'
+                con = sqlite3.connect('hw13.db')
+                con.row_factory = sqlite3.Row
+                cur = con.cursor()
+                # TODO: insecure, fix if have time
+                cur.execute(
+                    "INSERT INTO student (student_id, student_first_name, student_last_name) VALUES ({}, {}, {})".format(
+                        sid, sfn, sln))
+                con.commit()
+                con.close()
+                return redirect('/dashboard')
+            except:
+                error = 'Invalid Name. Please try again.'
+                return render_template('studentadd.html', error=error)
+        else:
+            return redirect(url_for('dashboard'))
+    else:
+        return render_template("studentadd.html")
+
+
+@app.route('/quiz/add', methods=['GET', 'POST'])
+def quizadd():
+    con = sqlite3.connect('hw13.db')
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("SELECT quiz_id FROM quizzes WHERE quiz_id=(SELECT max(quiz_id) FROM quizzes)")
+    quiz_id = [dict(id=row[0]) for row in cur.fetchall()][0]["id"] + 1
+    con.close()
+    if request.method == 'POST':
+        if request.form['quiz_subject'] is not None:
+            try:
+                subject = '"'+ request.form['quiz_subject'].strip() + '"'
+                q_amt =request.form['quiz_question_amount'].strip()
+                q_date = request.form['quiz_date'].strip()
+                q_date = '"'+ datetime.datetime.strptime(q_date, '%Y-%m-%d').strftime('%B %d,%Y') + '"'
+                q_date = q_date
+                print('quiz_id {}, subject {}, q_amt {} q_date {}'.format(quiz_id,subject,q_amt,q_date))
+                con = sqlite3.connect('hw13.db')
+                con.row_factory = sqlite3.Row
+                cur = con.cursor()
+                # TODO: insecure, fix if have time
+                cur.execute(
+                    "INSERT INTO quizzes (quiz_id, quiz_subject, quiz_question_amount, quiz_date) VALUES ({}, {}, {}, {})".format(quiz_id,subject,q_amt,q_date))
+                con.commit()
+                con.close()
+                return redirect('/dashboard')
+            except:
+                error = 'Invalid Name. Please try again.'
+                return render_template('quizadd.html', error=error)
+        else:
+            return redirect(url_for('dashboard'))
+    else:
+        return render_template("quizadd.html")
 
 
 if __name__ == '__main__':
