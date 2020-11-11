2cb369833f20a919f031bd37323884d15e0cc0ce
gamblingrepairman <awaite67@gmail.com>
2020-11-10 23:29:12 -0500

    part 3 done

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, g
 import logging
 from logging import FileHandler
 import os
-
+import sqlite3
 
 app = Flask(__name__)
 newlist = []
@@ -14,82 +14,44 @@ def logfile_start():
     app.logger.addHandler(file_handler)
 
 
-def sql_load_sample():
-    import sqlite3 as lite
-
-    def db_setup(query_str=str()):
-        conn = lite.connect('pet.db')
-        c = conn.cursor()
-        for l in query_str:
-            c.execute(l)
-            conn.commit()
-        conn.close()
-
-    def split_query(query_str=str(), split=str()):
-        query_split = query_str.split(split)
-        query_return = []
-        for l in query_split:
-            l = "INSERT INTO " + l.strip()
-            l = l.replace(':', ' VALUES')
-
-            query_return = query_return + [l.strip()]
-        query_return.pop()
-        return query_return
-
-    query = """Person: (1, 'James', 'Smith', 41)
-             Person: (2, 'Diana', 'Greene', 23)
-             Person: (3, 'Sara', 'White', 27)
-             Person: (4, 'William', 'Gibson', 23)
-             Pet: (1, 'Rusty', 'Dalmation', 4, 1)
-             Pet: (2, 'Bella', 'Alaskan Malamute', 3, 0)
-             Pet: (3, 'Max', 'Cocker Spaniel', 1, 0)
-             Pet: (4, 'Rocky', 'Beagle', 7, 0)
-             Pet: (5, 'Rufus', 'Cocker Spaniel', 1, 0)
-             Pet: (6, 'Spot', 'Bloodhound', 2, 1)
-             Person_Pet: (1, 1)
-             Person_Pet: (1, 2)
-             Person_Pet: (2, 3)
-             Person_Pet: (2, 4)
-             Person_Pet: (3, 5)
-             Person_Pet: (4, 6)"""
-
-    query = split_query(query, '\n')
-    for line in query:
-        print(line)
-    db_setup(query)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'password':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('dashboard'))
+    return render_template('login.html', error=error)
 
 
 @app.route('/')
-def hello_world():
-    logfile_start()
-    author = "author"
-    name = "name"
-    return render_template('index.html', author=author, name=name, fakelist=newlist)
+def indexpage():
+    con = sqlite3.connect('hw13.db')
+    con.row_factory = sqlite3.Row
+
+    cur = con.cursor()
+    cur.execute("select * from student")
+
+    rows = cur.fetchall()
+    return render_template("index2.html", rows=rows)
 
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    task = request.form['task']
-    email = request.form['email']
-    priority = request.form['priority']
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    con = sqlite3.connect('hw13.db')
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("INSERT OR IGNORE INTO student (student_id, student_first_name, student_last_name) VALUES (1,'John','Smith')")
+    cur.execute("INSERT OR IGNORE INTO quizzes (quiz_id, quiz_subject, quiz_question_amount, quiz_date) VALUES (1, \
+    'Python Basics', 5, 'February, 5th, 2015')")
+    cur.execute("select student.student_id, student.student_first_name, student.student_last_name, \
+                quizzes.quiz_id, quizzes.quiz_subject, quizzes.quiz_question_amount, quizzes.quiz_date \
+                from student, quizzes")
 
-    def check_items(task, email, priority):
-        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
-        if re.search(regex, email):
-            if priority in ['low', 'medium', 'high']:
-                newlist.append([task, email, priority])
-        else:
-            pass
-        return redirect('/')
-
-    check_items(task, email, priority)
-    return redirect('/')
-
-
-@app.route('/clear', methods=['POST'])
-def clear():
-    newlist.clear()
-    return redirect('/')
+    rows = cur.fetchall()
+    con.close()
+    return render_template("dashboard.html", rows=rows)
 
 
 
--- schema.sql
+++ schema.sql
@@ -22,3 +22,8 @@ CREATE TABLE score(
   	REFERENCES quizzes (quiz_id)
 );
 
+
+
+INSERT INTO student (student_id, student_first_name, student_last_name) VALUES (1,"John","Smith") ;
+INSERT INTO quizzes (quiz_id, quiz_subject, quiz_question_amount, quiz_date) VALUES (1, "Python Basics", 5, "February, 5th, 2015")
+SELECT score.student_id, student.student_first_name FROM score INNER JOIN student ON  score.student_id = student.student_id WHERE score.student_id = 2
